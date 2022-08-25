d8dd5f3a90b312a85410a533f5d2d74a759e05d6
Alex Rowe <alexrowe707@gmail.com>
2022-08-25 12:46:46 -0700

    Antidoe

--- .config/fzf.zsh
+++ .config/fzf.zsh
@@ -33,33 +33,58 @@ ctrlp () {
         opts="$FZF_TMUX_OPTS"
     fi;
 
-        # --bind "ctrl-p:change-prompt(Dirs> )+enable-search+reload(fd -d 1 -H -t d {q})" \
-    IFS=: read -r selected < <(
-    FZF_DEFAULT_COMMAND="fd -H" \
-        $cmd $opts --ansi \
-        --color "hl:-1:underline,hl+:-1:underline:reverse" \
-        --bind "ctrl-d:change-prompt(Dirs> )+enable-search+reload(fd -H -t d {q})" \
-        --bind "alt-d:change-prompt(Dirs> )+enable-search+reload(fd -d 1 -H -t d {q})" \
-        --bind "ctrl-f:change-prompt(Files> )+enable-search+reload(fd -H -t f {q})" \
-        --bind "alt-f:change-prompt(Files> )+enable-search+reload(fd -d 1 -H -t f {q})" \
-        --bind "ctrl-c:change-prompt(Commands> )+enable-search+reload(print -rC1 -- ${(ko)commands})" \
-        --bind "alt-t:change-prompt(Recent> )+enable-search+reload(fasd -al | grep $PWD)" \
-        --bind "ctrl-t:change-prompt(Recent> )+enable-search+reload(fasd -al)" \
-        --bind "ctrl-w:abort" \
-        --bind "ctrl-b:change-prompt(Files+Dirs> )+reload(fd -H {q})" \
-        --bind "alt-b:change-prompt(Files+Dirs> )+reload(fd -d 1 -H {q})" \
-        --bind "ctrl-e:accept+execute(echo _fzf_edit {1})" \
-        --bind "ctrl-s:accept+execute(echo _fzf_show {1})" \
-        --bind "ctrl-r:accept+execute(echo echo {1})" \
-        --bind "ctrl-u:accept+execute(echo echo cd .. && ctrlp)" \
-        --bind "ctrl-y:accept+execute(echo echo cd - && ctrlp)" \
-        --bind "ctrl-p:accept+execute(echo echo cd {} && ctrlp)" \
-        --bind "enter:accept+execute(echo echo echo {})" \
-        --prompt 'Files+Dirs> ' \
-        --delimiter : \
-        --header '[F]iles | [D]irs | [B]oth | [C]ommands | Recen[T] - [E]dit | [S]how | [R]un - [W] Close | [U]p | [Y] Back' \
-        --preview 'prev {1}' \
-        --preview-window 'up,60%,border-bottom,+{2}+3/3,~3'
-    )
-    [ -n "${selected}" ] && eval "${selected}"
+    while true; do
+        pwd=$PWD
+
+        IFS=: read -r selected < <(
+
+            $cmd $opts --ansi \
+            --color "hl:-1:underline,hl+:-1:underline:reverse" \
+            --bind "ctrl-d:change-prompt($PWD Dirs> )+enable-search+reload(fd -H -t d {q})" \
+            --bind "alt-d:change-prompt($PWD Dirs> )+enable-search+reload(fd -d 1 -H -t d {q})" \
+            --bind "ctrl-f:change-prompt($PWD Files> )+enable-search+reload(fd -H -t f {q})" \
+            --bind "alt-f:change-prompt($PWD Files> )+enable-search+reload(fd -d 1 -H -t f {q})" \
+            --bind "ctrl-c:change-prompt($PWD Commands> )+enable-search+reload(print -rC1 -- ${(ko)commands})" \
+            --bind "alt-t:change-prompt($PWD Recent> )+enable-search+reload(fasd -al | grep $PWD)" \
+            --bind "ctrl-t:change-prompt($PWD Recent> )+enable-search+reload(fasd -al)" \
+            --bind "ctrl-w:abort" \
+            --bind "ctrl-b:change-prompt($PWD Files+Dirs> )+reload(fd -H {q})" \
+            --bind "alt-b:change-prompt($PWD Files+Dirs> )+reload(fd -d 1 -H {q})" \
+            --bind "enter:accept+execute(echo _fzf_edit {1})" \
+            --bind "ctrl-s:accept+execute(echo _fzf_show {1})" \
+            --bind "ctrl-r:accept+execute(echo echo {1})" \
+            --bind "ctrl-u:accept+execute(echo CTRLP_UP)" \
+            --bind "ctrl-y:accept+execute(echo CTRLP_BACK)" \
+            --bind "ctrl-p:accept+execute(echo CTRLP_CD {})" \
+            --bind "ctrl-x:accept+execute(echo CTRLP_TMUX)" \
+            --bind "ctrl-e:accept+execute(echo echo echo {})" \
+            --prompt "$PWD Files+Dirs> " \
+            --delimiter : \
+            --header '[F]iles | [D]irs | [B]oth | [C]ommands | Recen[T] - [E]dit | [S]how | [R]un - [W] Close | [U]p | [Y] Back' \
+            --preview 'prev {1}' \
+            --preview-window 'right,60%,border-bottom,+{2}+3/3,~3'
+        )
+        case "$selected" in
+            "")
+                break
+                ;;
+            CTRLP_UP)
+                cd ..
+                ;;
+            CTRLP_BACK)
+                cd -
+                ;;
+            CTRLP_CD*)
+                cd "$(echo $selected | sed -e 's{.*CTRLP_CD {{g')"
+                ;;
+            CTRLP_TMUX)
+                cmd="fzf-tmux";
+                opts="$FZF_TMUX_OPTS"
+            ;;
+            *)
+                eval "${selected}"
+                break
+                ;;
+        esac
+    done;
 }
--- .config/nvim/init.vim
+++ .config/nvim/init.vim
@@ -1,6 +1,3 @@
-
-vim.cmd[[
-    set runtimepath^=~/.vim runtimepath+=~/.vim/after
-    let &packpath = &runtimepath
-    source ~/.vimrc
-]]
+set runtimepath^=~/.vim runtimepath+=~/.vim/after
+let &packpath = &runtimepath
+source ~/.vimrc
