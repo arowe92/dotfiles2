55e7ff6e7a37307e1deb7c767b9c038c00f6d029
Alexander Rowe <alexrowe707@gmail.com>
2024-09-12 16:40:22 -0700

    added markdown

--- lua/nvim_config/plugins/init.lua
+++ lua/nvim_config/plugins/init.lua
@@ -523,5 +523,29 @@ return {
             require("telescope").load_extension('harpoon')
         end
 
+    }, 
+
+    {
+        'MeanderingProgrammer/markdown.nvim',
+        name = 'render-markdown', 
+        -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+        dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+        dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+        config = function()
+            require('render-markdown').setup({})
+        end,
+    },
+
+    {
+        "scottmckendry/cyberdream.nvim",
+        lazy = false,
+        priority = 1000,    
+        config = function ()  
+            require("cyberdream").setup {
+                theme = {
+                    variant = "auto",
+                }
+            }
+        end
     }
 }
